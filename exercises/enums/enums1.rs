0445eebb48d9f24b20206c554bf5905d94085e95
Your Name <you@example.com>
2025-09-25 22:36:28 +0800

    111

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -7,11 +7,15 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
 }
 
 fn main() {
     println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Echo(String::from("hello world")));
+    println!("{:?}", Message::Move { x: 10, y: 30 });
+    println!("{:?}", Message::ChangeColor(200, 255, 255));
 }
